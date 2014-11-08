d563043a7f0538f937741e0c4824bec79319460f
buddhistpirate <buddhistpirate@chubtoad.com>
2014-11-08 10:19:30 -0500

    Auto width on main content on github

---
+++ widehub_blob.js
@@ -0,0 +1,3 @@
+$(".container").css("width", "auto")
+$(".repository-content").css("width", "96%")
+$(".container").css("margin", "20px")
---
+++ widehub_page.js
@@ -0,0 +1,3 @@
+$(".container").css("width", "auto")
+$(".repository-content").css("width", "85%")
+$(".container").css("margin", "20px")
